960e7396e24f3ce609f5e16f85d3e093918787c6
hyunseo <ols3040015@naver.com>
2025-05-28 17:52:16 +0900

    chore: remove .key files manually from latest commit

--- src/main/java/com/ddwu/hospital_reservation/controller/HomeController.java
+++ src/main/java/com/ddwu/hospital_reservation/controller/HomeController.java
@@ -10,6 +10,7 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("message", "JSP를 사용하는 Spring MVC 예제입니다!");
-        return "home"; // → /WEB-INF/views/home.jsp 렌더링됨
+        return "index"; 
     }
+    
 }
