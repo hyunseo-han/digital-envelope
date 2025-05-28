ba4451f4adc0c3aff01153de870532cccd9be82f
Juhee <pjh402941@gmail.com>
2025-05-28 18:14:24 +0900

    fix:key location 수정

--- src/main/java/com/ddwu/hospital_reservation/security/KeyManager.java
+++ src/main/java/com/ddwu/hospital_reservation/security/KeyManager.java
@@ -7,25 +7,30 @@ import java.util.Base64;
 
 public class KeyManager {
 
-    // Base64 인코딩된 개인키 로딩
-    public static PrivateKey loadPrivateKey(String filepath) throws Exception {
-        String key = readFromFile(filepath);
+    // 개인키 로딩 (classpath 기준)
+    public static PrivateKey loadPrivateKey(String filename) throws Exception {
+        String key = readFromResource("keys/" + filename);
         byte[] keyBytes = Base64.getDecoder().decode(key);
         PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         return keyFactory.generatePrivate(spec);
     }
 
-    // Base64 인코딩된 공개키 로딩
-    public static PublicKey loadPublicKey(String filepath) throws Exception {
-        String key = readFromFile(filepath);
+    // 공개키 로딩 (classpath 기준)
+    public static PublicKey loadPublicKey(String filename) throws Exception {
+        String key = readFromResource("keys/" + filename);
         byte[] keyBytes = Base64.getDecoder().decode(key);
         X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         return keyFactory.generatePublic(spec);
     }
 
-    private static String readFromFile(String path) throws IOException {
-        return new String(java.nio.file.Files.readAllBytes(new File(path).toPath()));
+    // classpath에서 파일 읽기
+    private static String readFromResource(String resourcePath) throws IOException {
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(resourcePath);
+        if (is == null) {
+            throw new FileNotFoundException("파일을 찾을 수 없습니다: " + resourcePath);
+        }
+        return new String(is.readAllBytes());
     }
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,4 +1,4 @@
-<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
@@ -6,5 +6,14 @@
 </head>
 <body>
     <h1>${message}</h1>
+
+    <form action="reserve" method="post">
+        이름: <input type="text" name="name"><br>
+        생년월일: <input type="text" name="birth"><br>
+        진료과: <input type="text" name="department"><br>
+        증상: <textarea name="symptom"></textarea><br>
+        예약일자: <input type="date" name="date"><br>
+        <input type="submit" value="예약 요청">
+    </form>
 </body>
 </html>
